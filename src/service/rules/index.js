8b60e0d5dca04fdfebdecb1c38d6aa9a8772e640
yhuan <cokekokotea@gmail.com>
2018-12-14 18:47:45 +0800

    添加 rule 时候的业务逻辑

--- src/service/rules/index.js
+++ src/service/rules/index.js
@@ -1,9 +1,26 @@
 import { axios } from '@http';
+import _ from 'lodash';
 
 export const getRulesList = () => {
   return axios.get('/rules');
 };
 
 export const saveRule = (formValue) => {
-  console.log(formValue);
+  const formBody = {};
+  let ruleName = '';
+
+  // 拼装符合 API 规定的数据格式
+  _.forIn(formValue, (val, key) => {
+    if (key.search('Keys') === -1) {
+      if (_.isString(val)) {
+        ruleName = val;
+      } else if (_.isArray(val)) {
+        if (val.length > 0 && val[0] !== undefined) {
+          formBody[key] = val;
+        }
+      }
+    }
+  });
+
+  return axios.put(`/rules/${ruleName}`, formBody);
 };
