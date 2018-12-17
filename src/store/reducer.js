aa9791c88092b798b2635f19744a581d3fa22a70
yhuan <cokekokotea@gmail.com>
2018-12-17 13:02:05 +0800

    添加 routers 模块的 store 部分初始代码

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,12 +3,14 @@ import { reducer as globalReducer } from '@common/store';
 import { reducer as ingressReducer } from '@pages/ingress/store';
 import { reducer as egressesReducer } from '@pages/egresses/store';
 import { reducer as rulesReducer } from '@pages/rules/store';
+import { reducer as routerReducer } from '@pages/routes/store';
 
 const reducer = combineReducers({
   global: globalReducer,
   ingress: ingressReducer,
   egresses: egressesReducer,
-  rules: rulesReducer
+  rules: rulesReducer,
+  router: routerReducer,
 });
 
 export default reducer;
