c712529af91e94ac4dfbcebfb468e20cad9183d6
yhuan <cokekokotea@gmail.com>
2018-12-24 15:16:00 +0800

    每次使用开发模式，先删除 build 文件，保证当有修改动作之后，开始使用而非开发时保证代码最新

--- scripts/dev-web.js
+++ scripts/dev-web.js
@@ -12,10 +12,26 @@ program
 
 const inputPort = program.p ? program.p : process.argv[2];
 
+function removeDirectory(path) {
+  if (fs.existsSync(path)) {
+    const files = fs.readdirSync(path);
+    files.forEach(fileName => {
+      const currFilePath = `${path}/${fileName}`;
+      if (fs.statSync(currFilePath).isDirectory()) {
+        removeDirectory(currFilePath);
+      } else {
+        fs.unlinkSync(currFilePath);
+      }
+    });
+
+    fs.rmdirSync(path);
+  }
+}
+
 if (inputPort) {
   const portReg = /^([0-9]|[1-9]\d|[1-9]\d{2}|[1-9]\d{3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])$/;
   if (portReg.test(inputPort)) {
-    const proxyConfigPath = path.join(__dirname, 'proxy-config.json');
+    const proxyConfigPath = `${process.cwd()}/config/proxy-config.json`;
     const proxyConfigWriteStream = fs.createWriteStream(proxyConfigPath);
     const config = {
       port: inputPort
@@ -28,6 +44,7 @@ if (inputPort) {
     });
 
     proxyConfigWriteStream.on('finish', () => {
+      removeDirectory(`${process.cwd()}/build`);
       exec('react-app-rewired start', (err, stdout, stderr) => {
         if (err) {
           console.log(err);
